7eb098b47dc26fd880f6380468e473a204431e48
StasSaltykov <stasyanych92@gmail.com>
2022-01-10 02:20:58 +0200

    homework #9

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 console.log("script launched");
+
 let detailsImage = document.querySelector(".details-image");
 let detailsTitle = document.querySelector(".details-title");
 let anchors = document.querySelectorAll(".thumbnails-anchor");//all HTML elements belonging to the clas thumbnails-anchor
 let mainContentEl = document.querySelector(".main-content");
 let selectedItem;
+let detailsBark = document.querySelector(".detailsBark");
+
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function(event) {
         event.preventDefault(); //canceling default behavior of anchor element hitting
@@ -22,16 +25,30 @@ function setDetails(anchor) {
     selectedItem = anchor.parentElement;
     // get element with class thumbnails-title inside the given anchor.
     let thumbnailsTitleSelector = `[href="${hrefValue}"] .thumbnails-title`;
+    let barkSoundSelector = `[href="${hrefValue}"] .barkSound`;
     let thumbnailsTitleEl = document.querySelector(thumbnailsTitleSelector);
+    let barkSoundEl = document.querySelector(barkSoundSelector);
     // dog name exists inside thumbnailsTitleEl.textContent.
     detailsTitle.textContent = `${thumbnailsTitleEl.textContent}: ${anchor.getAttribute('data-details-title')}`;
+    let sound = barkSoundEl.getAttribute("src");
+    detailsBark.setAttribute("src", sound);
+    detailsBark.muted = false;
+    setTimeout(muteUnmute, 2000);
 }
-function showDetails() {
+function showDetails(){
     mainContentEl.classList.remove('hidden');
+    detailsImage.parentElement.classList.add('is-tiny');
+    setTimeout(removeIsTiny);
 }
-function hideDetails() {
+function removeIsTiny(){
+    detailsImage.parentElement.classList.remove('is-tiny');;
+}
+function hideDetails(){
     mainContentEl.classList.add('hidden');
     if(mainContentEl.classList.contains("hidden")){
         selectedItem.classList.remove("selected");
     }
+}
+function muteUnmute(){
+    detailsBark.muted = true;
 }
